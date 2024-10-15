f63a406f359d9915b41172fe0df34f2716314cf5
Paul Young <paulyoung84@hotmail.co.uk>
2024-10-15 12:22:24 +0100

    Update googleApiFileList.py
    Added exportlinks to API request

--- googleApiFileList.py
+++ googleApiFileList.py
@@ -52,17 +52,18 @@ except ImportError:
 
 import pandas as pd
 
+
 # If modifying these scopes, delete your previously saved credentials
 
 # at ~/.credentials/drive-python-quickstart.json
 
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
 
 CLIENT_SECRET_FILE = 'credentials.json'
 
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
-folder_id = 'ADD GOOGLE FOLDER ID' #Set to id of the parent folder you want to list (should be content folder)
+folder_id = 'ADD FOLDER GOOGLE ID' #Set to id of the parent folder you want to list (should be content folder)
 folder_list = []
 all_folders = []
 file_list = []
@@ -88,7 +89,7 @@ def get_credentials():
 
     home_dir = os.path.expanduser('~')
 
-    credential_dir = os.path.join(home_dir, '.credentials')
+    credential_dir = os.path.join(home_dir, '.credentials3')
 
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
@@ -191,7 +192,7 @@ def get_file_list(): #runs over each folder generating file list, for files over
             results = service.files().list(
                 q="'" + folder + "' in parents",
 
-                pageSize=1000, fields="nextPageToken, files(name, md5Checksum, mimeType, size, createdTime, modifiedTime, id, parents, trashed, copyRequiresWriterPermission, shortcutDetails)", pageToken=page_token, supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+                pageSize=1000, fields="nextPageToken, files(name, md5Checksum, mimeType, size, createdTime, modifiedTime, id, parents, trashed, copyRequiresWriterPermission, shortcutDetails, exportLinks)", pageToken=page_token, supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
 
             items = results.get('files', [])
             for item in items:
@@ -215,6 +216,27 @@ def get_file_list(): #runs over each folder generating file list, for files over
 
                 copyRequiresWriterPermission = item.get('copyRequiresWriterPermission')
 
+                exportLinks = item.get('exportLinks')
+
+                if mimeType == 'application/vnd.google-apps.document' and exportLinks != None:
+                    standardDownloadLink = exportLinks['application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+                    PDFDownloadLink = exportLinks['application/pdf']
+                elif mimeType == 'application/vnd.google-apps.spreadsheet' and exportLinks != None:
+                    standardDownloadLink = exportLinks['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+                    PDFDownloadLink = exportLinks['application/pdf']
+                elif mimeType == 'application/vnd.google-apps.presentation' and exportLinks != None:
+                    standardDownloadLink = exportLinks['application/vnd.openxmlformats-officedocument.presentationml.presentation']
+                    PDFDownloadLink = exportLinks['application/pdf']
+                elif mimeType == 'application/vnd.google-apps.jam' and exportLinks != None:
+                    standardDownloadLink = exportLinks['application/pdf']
+                    PDFDownloadLink = ''
+                elif mimeType == 'application/vnd.google-apps.drawing' and exportLinks != None:
+                    standardDownloadLink = exportLinks['image/png']
+                    PDFDownloadLink = ''
+                else:
+                    standardDownloadLink = ''
+                    PDFDownloadLink = ''
+
                 ShortcutDetails = item.get('shortcutDetails')
 
                 if ShortcutDetails != None:
@@ -223,12 +245,38 @@ def get_file_list(): #runs over each folder generating file list, for files over
                         ShortcutMimeType = ShortcutDetails['targetMimeType']
 
                         Shortcuts = service.files().get(fileId=ShortcutID,
-                                                     fields = "copyRequiresWriterPermission, createdTime, modifiedTime", supportsAllDrives=True).execute()
+                                                     fields = "copyRequiresWriterPermission, exportLinks, createdTime, modifiedTime", supportsAllDrives=True).execute()
                         ShortcutCopyRequiresWritersPermissions = Shortcuts['copyRequiresWriterPermission']
-                  
+
                         ShortcutCreatedTime = Shortcuts['createdTime']
 
                         ShortcutModifiedTime = Shortcuts['modifiedTime']
+
+                        try:
+                            exportLinks = Shortcuts['exportLinks']
+                        except:
+                            pass
+
+                        if ShortcutMimeType == 'application/vnd.google-apps.document' and exportLinks != None:
+                            standardDownloadLink = exportLinks[
+                                'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+                            PDFDownloadLink = exportLinks['application/pdf']
+                        elif ShortcutMimeType == 'application/vnd.google-apps.spreadsheet' and exportLinks != None:
+                            standardDownloadLink = exportLinks[
+                                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+                            PDFDownloadLink = exportLinks['application/pdf']
+                        elif ShortcutMimeType == 'application/vnd.google-apps.presentation' and exportLinks != None:
+                            standardDownloadLink = exportLinks[
+                                'application/vnd.openxmlformats-officedocument.presentationml.presentation']
+                            PDFDownloadLink = exportLinks['application/pdf']
+                        elif ShortcutMimeType == 'application/vnd.google-apps.jam' and exportLinks != None:
+                            standardDownloadLink = exportLinks['application/pdf']
+                            PDFDownloadLink = ''
+                        elif ShortcutMimeType == 'application/vnd.google-apps.drawing' and exportLinks != None:
+                             standardDownloadLink = exportLinks['image/png']
+                             PDFDownloadLink = ''
+                        else:
+                            pass
 
                 else:
                         ShortcutID = None
@@ -236,12 +284,14 @@ def get_file_list(): #runs over each folder generating file list, for files over
                         ShortcutCopyRequiresWritersPermissions = None
                         ShortcutCreatedTime = None
                         ShortcutModifiedTime = None
-                file_list.append([name, checksum, mimeType, size, createdTime, modifiedTime, id, parents, trashed, copyRequiresWriterPermission, ShortcutID, ShortcutMimeType, ShortcutCreatedTime,ShortcutModifiedTime, ShortcutCopyRequiresWritersPermissions])
+
+
+                file_list.append([name, checksum, mimeType, size, createdTime, modifiedTime, id, parents, trashed, copyRequiresWriterPermission, ShortcutID, ShortcutMimeType, ShortcutCreatedTime,ShortcutModifiedTime, ShortcutCopyRequiresWritersPermissions, standardDownloadLink, PDFDownloadLink])
 
             page_token = results.get('nextPageToken', None)
             if page_token is None:
                 break
-    files = pd.DataFrame(file_list,columns=['file_name','checksum_md5','mimeType','size', 'date_created', 'date_last_modified','google_id', 'google_parent_id', 'trashed', 'copyRequiresWriterPermission', 'ShortcutID', 'ShortcutMimeType', 'shortcutCreatedTime', 'shortcutModifiedTime', 'ShortcutCopyRequiresWritersPermissions'])
+    files = pd.DataFrame(file_list,columns=['file_name','checksum_md5','mimeType','size', 'date_created', 'date_last_modified','google_id','google_parent_id', 'trashed', 'download_require_permission', 'ShortcutID', 'ShortcutMimeType','shortcutCreatedTime', 'shortcutModifiedTime', 'ShortcutCopyRequiresWritersPermissions','standardDownloadLink','PDFDownloadLink'])
     files.drop(files[files['trashed'] == True].index, inplace=True) #removes files which have True listed in trashed, these are files which had been moved to the recycle bin
     foldernumbers = files['mimeType'].str.contains('application/vnd.google-apps.folder').sum()
     filenumbers = (~files['mimeType'].str.contains('application/vnd.google-apps.folder')).sum()
@@ -257,4 +307,3 @@ if __name__ == '__main__':
     merge()
     print('Generating file metadata list')
     get_file_list()
-
